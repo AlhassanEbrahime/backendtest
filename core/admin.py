8fcf4e11e4bcbbed1da2847b3a3e43f911277000
Al-hasan ebraheem <sonanomail@gmail.com>
2024-11-06 10:31:17 +0200

    adding custom user

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,23 @@
 from store.models import Product
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseuserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseuserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2","email"),
+            },
+        ),
+    )
 
 
 class TagInline(GenericTabularInline):
--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('products/',views.product_list),
-    path('products/<int:id>',views.product_detail)
+    path('products/<int:id>',views.product_detail),
+    path('collections/<int:id>',views.collection_detail, name='collection-detail'), 
 ]
